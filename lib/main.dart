90158880594376aa71822bca2de975e012301ccd
Joshua Erinosho <joshuaerinosho@gmail.com>
2021-07-22 10:49:06 +0100

    Clean up

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:sample/Themes.dart';
 import 'package:sample/theme_provider.dart';
 import 'package:sample/widgets.dart';
-import 'constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,8 +14,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool isOn = true;
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -27,44 +24,11 @@ class _MyAppState extends State<MyApp> {
       ],
       child: Consumer<ThemeProvider>(
         builder: (ctx, themeObject, _) => MaterialApp(
-          title: 'Color test',
-          themeMode: themeObject.mode,
-          theme: lightTheme,
-          darkTheme: darkTheme,
-          home: Builder(builder: (context) {
-            final bool _darkModeEnabled =
-                Theme.of(context).brightness == Brightness.dark;
-            _darkModeEnabled ? isOn = false : isOn = true;
-            return Scaffold(
-                key: ValueKey('main page'),
-                backgroundColor: Theme.of(context).backgroundColor,
-                appBar: AppBar(
-                  title: Text('Background Color Test'),
-                  centerTitle: true,
-                  backgroundColor: Theme.of(context).primaryColor,
-                ),
-                body: SafeArea(
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: mainAxisAlignment,
-                      children: [
-                        RoundButton(
-                          key: ValueKey('Button'),
-                          onTap: () {
-                            Provider.of<ThemeProvider>(context, listen: false)
-                                .toggleMode();
-                            setState(() {
-                              isOn ? isOn = false : isOn = true;
-                            });
-                          },
-                          icon: isOn ? Icons.dark_mode : Icons.light_mode,
-                        )
-                      ],
-                    ),
-                  ),
-                ));
-          }),
-        ),
+            title: 'Color test',
+            themeMode: themeObject.mode,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            home: Home()),
       ),
     );
   }
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,4 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sample/constants.dart';
+import 'package:sample/theme_provider.dart';
+
+
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  
+bool isOn = true;
+  @override
+  Widget build(BuildContext context) {
+    final bool _darkModeEnabled =
+        Theme.of(context).brightness == Brightness.dark;
+    _darkModeEnabled ? isOn = false : isOn = true;
+
+    return Scaffold(
+        key: ValueKey('main page'),
+        backgroundColor: Theme.of(context).backgroundColor,
+        appBar: AppBar(
+          title: Text('Background Color Test'),
+          centerTitle: true,
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: mainAxisAlignment,
+              children: [
+                RoundButton(
+                  key: ValueKey('Button'),
+                  onTap: () {
+                    Provider.of<ThemeProvider>(context, listen: false)
+                        .toggleMode();
+                    setState(() {
+                      isOn ? isOn = false : isOn = true;
+                    });
+                  },
+                  icon: isOn ? Icons.dark_mode : Icons.light_mode,
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+
+
+
+
+
 class RoundButton extends StatelessWidget {
   const RoundButton({Key? key, required this.onTap, required this.icon})
       : super(key: key);
