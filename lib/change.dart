86f2fd3146c2c21a7c77fd0092c7010d12162874
Joshua Erinosho <joshuaerinosho@gmail.com>
2021-08-04 20:38:50 +0100

    Add code coverage

---
+++ lib/change.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Coverage extends StatelessWidget {
+  const Coverage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('We exclude this file from coverage'));
+  }
+}
